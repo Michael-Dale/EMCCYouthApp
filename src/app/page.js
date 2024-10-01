8f28260a9ccd58b118169082463ffcf20ba481b1
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-10-01 14:19:50 +0200

    Fixed Kinde auth bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,17 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-  
-  <>    
-  
-  <LoginLink>Sign in</LoginLink>
-  <RegisterLink>Sign up</RegisterLink>
-    <Navbar/>
-  
-  </>
-  
-
+    <>
+      {/* <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink> */}
+      <Navbar />
+    </>
   );
 }
