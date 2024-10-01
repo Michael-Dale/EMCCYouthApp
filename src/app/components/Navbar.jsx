e64d5574d1d904a511a04ddd6b5512a371863892
Michael Dale <mcdale0100@gmail.com>
2024-10-01 13:05:38 +0200

    V0 nav bar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import { Home, Search, PlusSquare, Heart, User } from "lucide-react";
-import Link from "next/link";
+import React from 'react'
+import { Home, Search, PlusSquare, Heart, User } from "lucide-react"
+import Link from "next/link"
 
 function NavbarIcon({ icon: Icon, label }) {
-  // Rename this to match the usage below
   return (
     <Link href="#" className="flex flex-col items-center text-white">
       <Icon className="w-6 h-6 mb-1" />
       <span className="text-xs">{label}</span>
     </Link>
-  );
+  )
 }
 
-export default function Navbar() {
+export default function Component() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-black p-4 ">
+    <nav className="fixed bottom-0 left-0 right-0 bg-black p-4 rounded-t-3xl">
       <div className="flex justify-around items-center max-w-screen-lg mx-auto">
-        <NavbarIcon icon={Home}/>{" "}
-        {/* Now matches the correct component name */}
-        <NavbarIcon icon={Search}/>
-        <NavbarIcon icon={PlusSquare}/>
-        <NavbarIcon icon={Heart}/>
-        <NavbarIcon icon={User}/>
+        <NavbarIcon icon={Home} label="Home" />
+        <NavbarIcon icon={Search} label="Search" />
+        <NavbarIcon icon={PlusSquare} label="Create" />
+        <NavbarIcon icon={Heart} label="Notifications" />
+        <NavbarIcon icon={User} label="Profile" />
       </div>
     </nav>
-  );
+  )
 }
