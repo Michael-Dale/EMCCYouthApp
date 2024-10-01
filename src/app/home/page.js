fe712bd515678692cd960f83da71e101ab8b8d51
Michael Dale <mcdale0100@gmail.com>
2024-10-01 14:43:45 +0200

    added home page

---
+++ src/app/home/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+      <div>
+        {/* Main content of your homepage goes here */}
+        <h1 className="text-center text-3xl font-bold mt-4">Welcome to My Website</h1>
+      </div>
+    );
+  }
