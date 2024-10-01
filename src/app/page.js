5218b411839bd5d9930abe106f80b7c7f8c66ed5
Michael Dale <mcdale0100@gmail.com>
2024-10-01 13:41:00 +0200

    added kinde, sign in not working

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
 
 export default function Home() {
   return (
+  
+  <>    
+  
+  <LoginLink>Sign in</LoginLink>
+  <RegisterLink>Sign up</RegisterLink>
     <Navbar/>
+  
+  </>
+  
+
   );
 }
