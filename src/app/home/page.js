f7c926368e76cdd0ba7a7668230c5057a8355667
Michael Dale <mcdale0100@gmail.com>
2024-10-02 15:18:36 +0200

    added whatsapp and insta links

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -9,6 +9,8 @@ export default function HomePage() {
         <h1 className="text-center text-3xl font-bold mt-4">Welcome to My Website</h1>
       </div>
        <Navbar />
+       <a href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" target="_blank">Send WhatsApp Message to Matthew</a>
+       <a href="https://www.instagram.com/connect_emcc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">Open instagram profile</a>
         </>
       
     );
