6b8dbf53788b3f450b9dd30be3bd5980eaa53dcc
Michael Dale <mcdale0100@gmail.com>
2024-10-02 18:59:35 +0200

    added social links and their respective svgs

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from "../components/Navbar.jsx";
+import SocialLinks from "../components/SocialLinks.jsx";
 
 export default function HomePage() {
     return (
@@ -9,9 +10,10 @@ export default function HomePage() {
         <h1 className="text-center text-3xl font-bold mt-4">Welcome to My Website</h1>
       </div>
        <Navbar />
-       <a href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" target="_blank">Send WhatsApp Message to Matthew</a>
+       {/* <a href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" target="_blank">Send WhatsApp Message to Matthew</a>
        <a href="" target="_blank">Youth Group</a>
-       <a href="https://www.instagram.com/connect_emcc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">Open instagram profile</a>
+       <a href="https://www.instagram.com/connect_emcc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">Open instagram profile</a> */}
+       <SocialLinks/>
 
        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSemUOkSVIMr6SWJCKqM8BNNpRdPLsq3SGGmgocVwuzKdwFP4A/viewform?embedded=true" width="640" height="1094" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </>
