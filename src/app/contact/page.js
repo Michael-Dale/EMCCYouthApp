4efb90c17570b6bac4a67222ba53c21e27f02736
Michael Dale <mcdale0100@gmail.com>
2024-10-03 12:04:11 +0200

    moved social links to contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,18 @@
 import PageTransition from "../components/PageTransition";
+import SocialLinks from "../components/SocialLinks.jsx";
 
 export default function Contact() {
   return (
-    <PageTransition>
+    <>
+     <PageTransition>
       <div className="p-4">
         <h1 className="text-2xl font-bold mb-4">Contact</h1>
         {/* Your contact page content here */}
       </div>
     </PageTransition>
+          <SocialLinks />
+    
+    </>
+   
   );
 }
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
-import SocialLinks from "../components/SocialLinks.jsx";
 import PageTransition from "../components/PageTransition.js";
 
 export default function HomePage() {
@@ -17,7 +16,6 @@ export default function HomePage() {
       {/* <a href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" target="_blank">Send WhatsApp Message to Matthew</a>
        <a href="" target="_blank">Youth Group</a>
        <a href="https://www.instagram.com/connect_emcc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">Open instagram profile</a> */}
-      <SocialLinks />
 
       {/* Commented out just because it was in the way  */}
       {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSemUOkSVIMr6SWJCKqM8BNNpRdPLsq3SGGmgocVwuzKdwFP4A/viewform?embedded=true" width="640" height="1094" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
