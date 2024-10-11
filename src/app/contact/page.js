ebb5664e35a0e046d3215831c8f9c56419511e47
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-10-11 11:20:29 +0200

    Contact Form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,18 +1,18 @@
 import PageTransition from "../components/PageTransition";
 import SocialLinks from "../components/SocialLinks.jsx";
+import Form from "../components/Form";
 
 export default function Contact() {
   return (
     <>
-     <PageTransition>
-      <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Contact</h1>
-        {/* Your contact page content here */}
-      </div>
-    </PageTransition>
-          <SocialLinks />
-    
+      <PageTransition>
+        <div className="p-4">
+          <h1 className="text-2xl font-bold mb-4">Contact</h1>
+          {/* Your contact page content here */}
+          <Form />
+        </div>
+      </PageTransition>
+      <SocialLinks />
     </>
-   
   );
 }
