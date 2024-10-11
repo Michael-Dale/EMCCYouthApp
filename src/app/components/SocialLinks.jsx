50f11a63f9c48917def8f65cb3d2a318c33b5df6
Michael Dale <mcdale0100@gmail.com>
2024-10-11 11:32:56 +0200

    added youtube link

--- src/app/components/SocialLinks.jsx
+++ src/app/components/SocialLinks.jsx
@@ -9,6 +9,10 @@ const instagram = () => (
     <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" width = "40px" height="40px"><path d="M349.33 69.33a93.62 93.62 0 0193.34 93.34v186.66a93.62 93.62 0 01-93.34 93.34H162.67a93.62 93.62 0 01-93.34-93.34V162.67a93.62 93.62 0 0193.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z" fill = "black"/><path d="M377.33 162.67a28 28 0 1128-28 27.94 27.94 0 01-28 28zM256 181.33A74.67 74.67 0 11181.33 256 74.75 74.75 0 01256 181.33m0-37.33a112 112 0 10112 112 112 112 0 00-112-112z" fill = "black"/></svg>
 );
 
+const youtube = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" width = "40px" height = "40px"><path d="M508.64 148.79c0-45-33.1-81.2-74-81.2C379.24 65 322.74 64 265 64h-18c-57.6 0-114.2 1-169.6 3.6C36.6 67.6 3.5 104 3.5 149 1 184.59-.06 220.19 0 255.79q-.15 53.4 3.4 106.9c0 45 33.1 81.5 73.9 81.5 58.2 2.7 117.9 3.9 178.6 3.8q91.2.3 178.6-3.8c40.9 0 74-36.5 74-81.5 2.4-35.7 3.5-71.3 3.4-107q.34-53.4-3.26-106.9zM207 353.89v-196.5l145 98.2z" fill-rule="evenodd" fill = "red"/></svg>
+);
+
 function NavbarIcon({ icon: Icon, href, target, label }) {
     return (
       <a href={href} target={target} className="flex flex-col items-center text-white">
@@ -35,6 +39,12 @@ function NavbarIcon({ icon: Icon, href, target, label }) {
           <NavbarIcon 
             icon={instagram} 
             href="https://www.instagram.com/connect_emcc?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" 
+            target="_blank" 
+          />
+
+          <NavbarIcon 
+            icon={youtube} 
+            href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" 
             target="_blank" 
           />
         </div>
