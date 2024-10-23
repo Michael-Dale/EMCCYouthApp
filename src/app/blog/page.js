f7c94d38c8464b70b3562143421a5dbbea998148
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-10-23 13:29:51 +0200

    Added a link

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,7 @@
 import PageTransition from "../components/PageTransition";
-import DevotionalPost from '../components/DevotionalPost';
-
+import DevotionalPost from "../components/DevotionalPost";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Blog() {
   return (
@@ -9,19 +10,19 @@ export default function Blog() {
         <h1 className="text-2xl font-bold mb-4">Blog</h1>
         {/* Your contact page content here */}
         <DevotionalPost
-        verse="Philippians 4:13 - I can do all things through Christ who strengthens me."
-        message="Remember, with faith and determination, you can overcome any challenge."
-        date="2024/10/11"
-        username="ConnectYouth"
-        profilePic="/icons/connect youth logo.svg"
-      />
+          verse="Philippians 4:13 - I can do all things through Christ who strengthens me."
+          message="Remember, with faith and determination, you can overcome any challenge."
+          date="2024/10/11"
+          username="ConnectYouth"
+          profilePic="/icons/connect youth logo.svg"
+        />
         <DevotionalPost
-        verse="Isaiah 41:10 
+          verse="Isaiah 41:10 
 “Fear not, for I am with you;
 Be not dismayed, for I am your God. 
 I will strengthen you,
 I will uphold you with My righteous right hand”"
-        message="What an amazing promise! God is telling you not to fear or be dismayed. Dismay is another word for distress (extreme anxiety, sorrow or pain) Why shouldn’t you fear or be dismayed? Because He is always with you! There is no reason to fear, regardless of your situation or circumstances. God is always with you, and He is telling you in this scripture He won’t leave you alone. 
+          message="What an amazing promise! God is telling you not to fear or be dismayed. Dismay is another word for distress (extreme anxiety, sorrow or pain) Why shouldn’t you fear or be dismayed? Because He is always with you! There is no reason to fear, regardless of your situation or circumstances. God is always with you, and He is telling you in this scripture He won’t leave you alone. 
 
 God promises that He will strengthen and help you! When you’re feeling like you can’t go on or you just don’t know how you’re going to get through the day, God will be right with you to provide you with the strength and help you need, to get through whatever you’re facing.
 
@@ -34,27 +35,33 @@ Breath Prayers are short, simple prayers inspired by scripture. Breath Prayers h
 Breath Prayer: Isaiah 41:10
 Inhale: I won’t be afraid
 Exhale: For you are with me"
-        date="2024/10/11"
-        username="ConnectYouth"
-        profilePic="/icons/connect youth logo.svg"
-      />
+          date="2024/10/11"
+          username="ConnectYouth"
+          profilePic="/icons/connect youth logo.svg"
+        />
 
-<DevotionalPost
-        verse="Isaiah 41:10 
+        <DevotionalPost
+          verse="Isaiah 41:10 
 “Fear not, for I am with you;
 Be not dismayed, for I am your God. 
 I will strengthen you,
 I will uphold you with My righteous right hand”"
-        message="Remember, with faith and determination, you can overcome any challenge.
+          message="Remember, with faith and determination, you can overcome any challenge.
 Remember, with faith and determination, you can overcome any challenge.
 Remember, with faith and determination, you can overcome any challenge.
 Remember, with faith and determination, y."
-        date="2024/10/11"
-        username="ConnectYouth"
-        profilePic="/icons/connect youth logo.svg"
-      />
-
+          date="2024/10/11"
+          username="ConnectYouth"
+          profilePic="/icons/connect youth logo.svg"
+        />
       </div>
+      <Link
+        href="/blog/devotions"
+        className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black rounded-md transition-colors duration-300 ease-in-out"
+      >
+        <span>View all devotions</span>
+        <ArrowRight className="ml-2 h-4 w-4" />
+      </Link>
     </PageTransition>
   );
 }
