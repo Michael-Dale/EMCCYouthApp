23978c57b0d9690c730f7d788049c68f2c727a9c
Michael Dale <mcdale0100@gmail.com>
2024-10-23 13:46:05 +0200

    added latest devotion to home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar.jsx";
 import EventCard from "../components/EventCard.jsx";
 import YouTubeLatestVideo from "../components/YouTubeLatestVideo"; // Adjust the path as necessary
 import ImageCarousel from '../components/ImageCarousel.jsx';
+import DevotionalPost from "../components/DevotionalPost.jsx";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 import PageTransition from "../components/PageTransition.js";
 const img = "/pics/Facebook.png";
@@ -37,6 +39,34 @@ export default function HomePage() {
             location={"ELOHIM CHURCH"}
           />
              <YouTubeLatestVideo />
+
+             <div className="border border-gray-300 rounded-2xl p-4 shadow-md max-w-lg mx-auto my-4 bg-white transition-shadow duration-200 hover:shadow-lg">
+      <h2 className="text-gray-800 text-2xl font-semibold">Latest Devotion</h2>
+      <DevotionalPost
+          verse="Isaiah 41:10 
+“Fear not, for I am with you;
+Be not dismayed, for I am your God. 
+I will strengthen you,
+I will uphold you with My righteous right hand”"
+          message="Remember, with faith and determination, you can overcome any challenge.
+Remember, with faith and determination, you can overcome any challenge.
+Remember, with faith and determination, you can overcome any challenge.
+Remember, with faith and determination, y."
+          date="2024/10/11"
+          username="ConnectYouth"
+          profilePic="/icons/connect youth logo.svg"
+        />
+      <div className="text-center mt-4">
+        <Link
+          href="/blog/devotions"
+          className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white rounded-md transition-colors duration-300 ease-in-out"
+        >
+          <span>View all devotions</span>
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </Link>
+      </div>
+
+    </div>
         </div>
       </PageTransition>
       {/* <a href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" target="_blank">Send WhatsApp Message to Matthew</a>
