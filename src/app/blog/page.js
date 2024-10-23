811a526dae105d4057ddd4c6c71f8202e77fda61
Michael Dale <mcdale0100@gmail.com>
2024-10-23 14:08:39 +0200

    added wrapper for testimony and sermons /blog and /home

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,5 +1,6 @@
 import PageTransition from "../components/PageTransition";
 import DevotionalPost from "../components/DevotionalPost";
+import PdfThumbnail from "../components/PdfThumbnail";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 
@@ -31,6 +32,38 @@ I will uphold you with My righteous right hand”`}
       className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white rounded-md transition-colors duration-300 ease-in-out"
     >
       <span>View all devotions</span>
+      <ArrowRight className="ml-2 h-4 w-4" />
+    </Link>
+  </div>
+</div>
+
+<div className="border border-gray-300 rounded-2xl p-4 shadow-md max-w-md mx-auto my-4 bg-white transition-shadow duration-200 hover:shadow-lg">
+  <h2 className="text-gray-800 text-2xl font-semibold text-center">Latest Sermon</h2>
+  <PdfThumbnail
+        pdfUrl="/docs/Beginners Guide Coding.pdf"  // Replace with your actual PDF path
+        title="Sample PDF 1"
+        imgSrc="/pics/img.jpg" 
+      />
+  <div className="text-center mt-4">
+    <Link
+      href="/blog/sermons"
+      className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white rounded-md transition-colors duration-300 ease-in-out"
+    >
+      <span>View all sermons</span>
+      <ArrowRight className="ml-2 h-4 w-4" />
+    </Link>
+  </div>
+</div>
+
+<div className="border border-gray-300 rounded-2xl p-4 shadow-md max-w-md mx-auto my-4 bg-white transition-shadow duration-200 hover:shadow-lg">
+  <h2 className="text-gray-800 text-2xl font-semibold text-center">Latest Testimony</h2>
+ 
+  <div className="text-center mt-4">
+    <Link
+      href="/blog/testimonies"
+      className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white rounded-md transition-colors duration-300 ease-in-out"
+    >
+      <span>View all testimonies</span>
       <ArrowRight className="ml-2 h-4 w-4" />
     </Link>
   </div>
