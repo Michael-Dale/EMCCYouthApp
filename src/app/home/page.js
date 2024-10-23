873d0a811573cf9ed9e17b42a11ac4e99d5f1f99
Michael Dale <mcdale0100@gmail.com>
2024-10-23 13:50:42 +0200

    tweaked devotion wrapper div and removed placeholder content

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -40,33 +40,34 @@ export default function HomePage() {
           />
              <YouTubeLatestVideo />
 
-             <div className="border border-gray-300 rounded-2xl p-4 shadow-md max-w-lg mx-auto my-4 bg-white transition-shadow duration-200 hover:shadow-lg">
-      <h2 className="text-gray-800 text-2xl font-semibold">Latest Devotion</h2>
-      <DevotionalPost
-          verse="Isaiah 41:10 
+             <div className="border border-gray-300 rounded-2xl p-4 shadow-md max-w-md mx-auto my-4 bg-white transition-shadow duration-200 hover:shadow-lg">
+  <h2 className="text-gray-800 text-2xl font-semibold text-center">Latest Devotion</h2>
+  <DevotionalPost
+    verse={`Isaiah 41:10 
 “Fear not, for I am with you;
 Be not dismayed, for I am your God. 
 I will strengthen you,
-I will uphold you with My righteous right hand”"
-          message="Remember, with faith and determination, you can overcome any challenge.
-Remember, with faith and determination, you can overcome any challenge.
-Remember, with faith and determination, you can overcome any challenge.
-Remember, with faith and determination, y."
-          date="2024/10/11"
-          username="ConnectYouth"
-          profilePic="/icons/connect youth logo.svg"
-        />
-      <div className="text-center mt-4">
-        <Link
-          href="/blog/devotions"
-          className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white rounded-md transition-colors duration-300 ease-in-out"
-        >
-          <span>View all devotions</span>
-          <ArrowRight className="ml-2 h-4 w-4" />
-        </Link>
-      </div>
+I will uphold you with My righteous right hand”`}
+    message="Remember, with faith and determination, you can overcome any challenge.
+    Remember, with faith and determination, you can overcome any challenge.
+    Remember, with faith and determination, you can overcome any challenge.
+    Remember, with faith and determination, y."
+    date="2024/10/11"
+    username="ConnectYouth"
+    profilePic="/icons/connect youth logo.svg"
+  />
+  <div className="text-center mt-4">
+    <Link
+      href="/blog/devotions"
+      className="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 text-sm font-medium text-black hover:bg-black hover:text-white rounded-md transition-colors duration-300 ease-in-out"
+    >
+      <span>View all devotions</span>
+      <ArrowRight className="ml-2 h-4 w-4" />
+    </Link>
+  </div>
+</div>
 
-    </div>
+
         </div>
       </PageTransition>
       {/* <a href="https://wa.me/27638074854?text=Hello%2C%20how%20are%20you%3F" target="_blank">Send WhatsApp Message to Matthew</a>
