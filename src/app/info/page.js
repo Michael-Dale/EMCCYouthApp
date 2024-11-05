b42647ad4836a321fb2406a16c85873abfd19828
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-11-05 15:15:45 +0200

    youtube offline draft

--- src/app/info/page.js
+++ src/app/info/page.js
@@ -1,6 +1,7 @@
 import PageTransition from "../components/PageTransition";
 const sender = require("../components/Emailer");
 import DirectionCard from "../components/DirectionCard";
+import YouTubeEmbed from "../components/YouTubevideo";
 export default function Info() {
   // sender();
   return (
@@ -9,6 +10,7 @@ export default function Info() {
         <h1 className="text-2xl font-bold mb-4">Info</h1>
         {/* Your contact page content here */}
         <DirectionCard />
+        <YouTubeEmbed />
       </div>
     </PageTransition>
   );
