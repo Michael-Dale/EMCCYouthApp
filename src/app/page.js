4a5f4abab1c9742a18ba71b36b58723bd895b143
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-11-28 14:29:57 +0200

    Made a temp landing page. Needs work

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,34 @@ import {
   LogoutLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-export default async function Home() {
-  const { isAuthenticated } = getKindeServerSession();
-  const isAuthed = await isAuthenticated();
+export default function Home() {
   return (
-    <>
-      <nav className="flex justify-between items-center py-8 px-24">
-        <h1 className="font-bold text-2xl">EMCC</h1>
-        {isAuthed ? (
-          <LogoutLink>Logout</LogoutLink>
-        ) : (
-          <div className="flex gap-2">
-            <LoginLink>Login</LoginLink>
-            <RegisterLink>Register</RegisterLink>
-          </div>
-        )}
-      </nav>
-      <h1 className="text-center text-3xl font-bold mt-4">Please login</h1>
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black flex flex-col items-center justify-center p-4">
+      <div className="absolute top-8 left-8">
+        <div className="text-white text-2xl font-bold">
+          <span className="text-teal-500">Connect</span>Youth
+        </div>
+      </div>
+      <div className="text-center max-w-xl mx-auto">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-4 sm:mb-8">
+          Welcome to Connect
+        </h1>
+        <p className="text-lg sm:text-xl text-gray-300 mb-8 sm:mb-12">
+          Join us and experience the next level of innovation.
+        </p>
+        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center">
+          <RegisterLink>
+            <button className="bg-teal-400 w-40 hover:bg-teal-500 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+              Sign Up
+            </button>
+          </RegisterLink>
+          <LoginLink>
+            <button className="bg-gray-700 w-40 hover:bg-gray-600 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+              Login
+            </button>
+          </LoginLink>
+        </div>
+      </div>
+    </div>
   );
 }
